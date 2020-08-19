268f2f102f1209994c9e3cc6d76bfef9c5ac94d1
abrahamyan44 <yurabrahamyan44@gmail.com>
2020-08-19 12:11:22 +0400

    modifications in hy

--- spacy/lang/hy/__init__.py
+++ spacy/lang/hy/__init__.py
@@ -12,11 +12,17 @@ from ...language import Language
 class ArmenianDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters[LANG] = lambda text: "hy"
-
     lex_attr_getters.update(LEX_ATTRS)
+
+    tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
     stop_words = STOP_WORDS
     tag_map = TAG_MAP
 
+    @classmethod
+    def create_lemmatizer(cls, nlp=None, lookups=None):
+        if lookups is None:
+            lookups = Lookups()
+        return ArmenianLemmatizer(lookups)
 
 class Armenian(Language):
     lang = "hy"
---
+++ spacy/lang/hy/tokenizer_exceptions.py
@@ -0,0 +1,45 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from ...symbols import ORTH, LEMMA, NORM
+
+
+_exc = {}
+
+_abbrev_exc = [
+    # Weekdays abbreviations
+    {ORTH: "երկ", LEMMA: "երկուշաբթի", NORM: "երկուշաբթի"},
+    {ORTH: "երք", LEMMA: "երեքշաբթի", NORM: "երեքշաբթի"},
+    {ORTH: "չրք", LEMMA: "չորեքշաբթի", NORM: "չորեքշաբթի"},
+    {ORTH: "հնգ", LEMMA: "հինգշաբթի", NORM: "հինգշաբթի"},
+    {ORTH: "ուրբ", LEMMA: "ուրբաթ", NORM: "ուրբաթ"},
+    {ORTH: "շբթ", LEMMA: "շաբաթ", NORM: "շաբաթ"},
+    {ORTH: "կիր", LEMMA: "կիրակի", NORM: "կիրակի"},
+    # Months abbreviations
+    {ORTH: "հնվ", LEMMA: "հունվար", NORM: "հունվար"},
+    {ORTH: "փետ", LEMMA: "փետրվար", NORM: "փետրվար"},
+    {ORTH: "մրտ", LEMMA: "մարտ", NORM: "մարտ"},
+    {ORTH: "ապր", LEMMA: "ապրիլ", NORM: "ապրիլ"},
+    {ORTH: "մայ", LEMMA: "մայիս", NORM: "մայիս"},
+    {ORTH: "հուն", LEMMA: "հունիս", NORM: "հունիս"},
+    {ORTH: "հուլ", LEMMA: "հուլիս", NORM: "հուլիս"},
+    {ORTH: "օգս", LEMMA: "օգոստոս", NORM: "օգոստոս"},
+    {ORTH: "սեպ", LEMMA: "սեպտեմբեր", NORM: "սեպտեմբեր"},
+    {ORTH: "հոկ", LEMMA: "հոկտեմբեր", NORM: "հոկտեմբեր"},
+    {ORTH: "նոյ", LEMMA: "նոյեմբեր", NORM: "նոյեմբեր"},
+    {ORTH: "դեկ", LEMMA: "դեկտեմբեր", NORM: "դեկտեմբեր"},
+]
+
+
+for abbrev_desc in _abbrev_exc:
+    abbrev = abbrev_desc[ORTH]
+    for orth in (abbrev, abbrev.capitalize(), abbrev.upper()):
+        _exc[orth] = [{ORTH: orth, LEMMA: abbrev_desc[LEMMA], NORM: abbrev_desc[NORM]}]
+        _exc[orth + "."] = [
+            {ORTH: orth + ".", LEMMA: abbrev_desc[LEMMA], NORM: abbrev_desc[NORM]}
+        ]
+
+
+
+
+TOKENIZER_EXCEPTIONS = _exc
